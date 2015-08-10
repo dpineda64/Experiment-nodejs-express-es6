aa7061d219862742be6e386f959aae1881bec414
Daniel Pineda <dpineda@guatuneed.com>
2015-08-10 15:20:26 -0600

    changed to jsonwebtokens interceptor

--- System/Helpers/interceptors.js
+++ System/Helpers/interceptors.js
@@ -1,9 +1,45 @@
+/*jslint node: true , esnext: true , globals : true*/
+'use strict';
 //
 //  Check if is logged in
 //
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+
 exports.isLoggedIn = (req , res, next) => {
-  if(req.isAuthenticated){
-    return next();
+  const token = req.body.token || req.query.token || req.headers['x-access-token'];
+  const secret = fs.readFileSync(_config.secretFile, 'utf-8');
+  if(token){
+    jwt.verify(token , secret , (err , decoded) => {
+      if(err){
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.status(403).send({
+      success : false,
+      message : 'no token provided'
+    });
   }
-  return res.redirect('/login');
+};
+
+
+// LocalStorage Manage
+class isLoggedInLocal {
+  constructor() {
+    this.name = 'handle LocalStorage Authentication';
+  }
+  getLocalStorage(){
+    return Window.localStorage.getItem('token');
+  }
+  setLocalStorage(token){
+    return Window.localStorage.setItem('token' , token);
+  }
 }
+
+const isloggedinlocal = new isLoggedInLocal();
+
+exports.isloggedinlocal = isloggedinlocal;
