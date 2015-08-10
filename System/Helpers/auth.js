cfb5db0331fe8646ad37cdeb58446db68773de81
Daniel Pineda <dpineda@guatuneed.com>
2015-08-10 12:26:18 -0600

    first commit

---
+++ System/Helpers/auth.js
@@ -0,0 +1,27 @@
+//
+//  Authentication Functions
+//
+
+const bcrypt = require('bcrypt-nodejs');
+
+class Auth {
+  constructor() {
+    this.name = 'Authentication helper';
+  }
+  generateHash(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  }
+  validPass(password , user_pass){
+    return bcrypt.compareSync(password, user_pass);
+  }
+}
+
+class generateHash extends Auth {
+  constructor() {
+    super();
+  }
+}
+
+const auth = new Auth();
+
+module.exports = auth;
